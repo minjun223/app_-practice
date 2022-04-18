9f9e7149012b6f1b0ecfb1512a1ce13a81e8644d
김민준 <80810346+minjun223@users.noreply.github.com>
2022-04-18 10:09:55 +0900

    my android pretice

--- app/src/main/java/com/example/pretice/ThreadActivity.kt
+++ app/src/main/java/com/example/pretice/ThreadActivity.kt
@@ -2,10 +2,38 @@ package com.example.pretice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_thread.*
 
 class ThreadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thread)
+
+        val runnable: Runnable = object : Runnable {
+            override fun run() {
+                Log.d("thread-1", "Thread1 is made")
+            }
+        }
+        val thread: Thread = Thread(runnable)
+        button6.setOnClickListener{
+            thread.start()
+        }
+
+        Thread(object : Runnable{
+            override fun run() {
+                Log.d("thread-1", "Thread2 is made")
+
+            }
+        })
+        Thread(Runnable {
+            Thread.sleep(2000)
+            Log.d("thread-1", "Thread3 is made")
+            runOnUiThread{
+                button6.setBackgroundColor(getColor(R.color.teal_700))
+            }
+        }).start()
+
+
     }
 }
