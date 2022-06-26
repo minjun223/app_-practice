2ad723a12e878c76b780a83bb1191944c173c1ca
김민준 <80810346+minjun223@users.noreply.github.com>
2022-06-26 15:43:45 +0900

    my android pretice

--- app/src/main/java/com/example/pretice/intent1.kt
+++ app/src/main/java/com/example/pretice/intent1.kt
@@ -7,26 +7,27 @@ import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
-class intent : AppCompatActivity() {
+class intent1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         change_activity.setOnClickListener{
-//            val intent = Intent(this@intent, intent2::class.java)
-//            intent.putExtra("number1",1)
-//            intent.putExtra("number2",2)
-//            startActivity(intent)
-//
-//            val intent2 = Intent(this@intent, intent2::class.java)
-//            intent2.apply {
-//                this.putExtra("number1", 1)
-//                this.putExtra("number2",1)
-//            }
-//            startActivityForResult(intent2,200)
-            val intent = Intent(Intent.ACTION_VIEW,Uri.parse("http://m.naver.com"))
+            val intent = Intent(this@intent1, intent2::class.java)
+            intent.putExtra("number1",1)
+            intent.putExtra("number2",2)
             startActivity(intent)
+
+            val intent2 = Intent(this@intent1, intent2::class.java)
+            intent2.apply {
+                this.putExtra("number1", 1)
+                this.putExtra("number2",1)
+            }
+            startActivityForResult(intent2,200)
+            //암시적 인탠트
+//            val intent = Intent(Intent.ACTION_VIEW,Uri.parse("http://m.naver.com"))
+//            startActivity(intent)
         }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
