55076386bdbdbb6007366a6abd3cda7408555c82
김민준 <80810346+minjun223@users.noreply.github.com>
2022-07-13 08:19:03 +0900

    Update LibraryActivity.kt

--- app/src/main/java/com/example/pretice/LibraryActivity.kt
+++ app/src/main/java/com/example/pretice/LibraryActivity.kt
@@ -8,6 +8,14 @@ class LibraryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
 
-//        Glibe.with(this@Li)
+    /* Glide.with(this@LibraryActivity)
+           .load("https://image.laftel" +
+                   ".net/items.thumbs/large/f3203d2d-13e7-43fc-afb0-a43a9a5fe56b.jpg")
+           .into(glide)
+           .centCrop()
+        Glide.with(this@LibraryActivity)
+            .load("https://image.laftel" +
+                    ".net/items.thumbs/large/f3203d2d-13e7-43fc-afb0-a43a9a5fe56b.jpg")
+            .into(glide) */
     }
 }
